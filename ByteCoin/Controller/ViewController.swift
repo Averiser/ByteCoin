92a561c2a2327afadf8113ea22f925a9fe6e0397
Averiser <averiser@gmail.com>
2021-09-07 19:19:45 +0300

    fourth commit gelegating, extenstions

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,25 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController: UIViewController {
   
   @IBOutlet weak var bitcoinLabel: UILabel!
   @IBOutlet weak var currencyLabel: UILabel!
   @IBOutlet weak var currencyPicker: UIPickerView!
   
+  //Need to change this to a var to be able to modify its properties.
+  var coinManager = CoinManager()
+  
     override func viewDidLoad() {
         super.viewDidLoad()
       
+      //Easily Missed: Must set the coinManager's delegate as this current class so that we can recieve
+      //the notifications when the delegate methods are called.
+      
+      coinManager.delegate = self
       currencyPicker.dataSource = self
       currencyPicker.delegate = self
     }
+}
+  //MARK: - CoinManagerDelegate
   
-  let coinManager = CoinManager()
+  extension ViewController: CoinManagerDelegate {
+    
+    //Provide the implementation for the delegate methods.
+    //When the coinManager gets the price it will call this method and pass over the price and currency.
+    
+    func didUpdatePrice(price: String, currency: String) {
+      
+      //Remember that we need to get hold of the main thread to update the UI, otherwise our app will crash if we
+      //try to do this from a background thread (URLSession works in the background).
+      
+      DispatchQueue.main.async {
+        self.bitcoinLabel.text = price
+        self.currencyLabel.text = currency
+      }
+    }
+    
+    func didFailWithError(error: Error) {
+      print(error)
+    }
+  }
+  
+
+  //MARK: - UIPickerView Delegate & DataSource
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
   
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
     return 1
   }
-  
   
   func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
     
@@ -41,7 +73,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     let selectedCurrency = coinManager.currencyArray[row]
     coinManager.getCoinPrice(for: selectedCurrency)
   }
-
-
 }
+
 
